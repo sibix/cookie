20ef203e4ca1a9bd7809f5d31c882a5a1701bf6e
sibi xavier <sibixavier@live.com>
2014-12-12 09:41:59 +0530

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -6,6 +6,36 @@
 
 	//Display various items available for shopping
 
+	echo "<html>";
+
+		echo "<head>";
+			echo "<title>Homepage</title>";
+		echo "</head>";
+
+		echo "<body>";
+
+				echo "<align='right'>";
+
+			//Welcome address with name
+			echo "Welcome $logcookie <br>";
+			echo "<a href='logout.php'>Logout</a>";
+
+			echo "<center>";		
+			echo "<h3>Shoppers paradise</h3>";
+			echo "<h5>Shop till you drop!!!</h5>";
+			echo "<br>";
+			echo "<br>";
+
+			echo "<table>";
+				echo "<tr align='center'>";
+
+
+
+			echo "</table>";
+
+		echo "</body>";
+
+	echo "</html>";
 	
 
 
